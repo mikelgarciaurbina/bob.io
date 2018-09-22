a4e0fa5e54032c12aefe49a5ce80826bc65c3791
Mikel Garcia <mikel_6@hotmail.com>
2018-09-22 12:26:27 +0200

    Add state to NewUser

--- client/src/containers/NewUser/NewUser.js
+++ client/src/containers/NewUser/NewUser.js
@@ -4,12 +4,31 @@ import { Input } from '../../components';
 
 import { Container, Title } from './components';
 
-const NewUser = () => (
-  <Container>
-    <Title>New User</Title>
-    <Input label="Name" />
-    <Input label="Lastname" />
-  </Container>
-);
+class NewUser extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { lastname: 'a', name: '' };
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(name) {
+    return ({ target }) => {
+      this.setState({ [name]: target.value });
+    };
+  }
+
+  render() {
+    const { lastname, name } = this.state;
+
+    return (
+      <Container>
+        <Title>New User</Title>
+        <Input label="Name" onChange={this.handleChange('name')} value={name} />
+        <Input label="Lastname" onChange={this.handleChange('lastname')} value={lastname} />
+      </Container>
+    );
+  }
+}
 
 export default NewUser;
